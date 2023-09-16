0a630b75732bb6f45da01cd76f26aafc9ce75cfe
RamyaRaghuveera <ramyarbhat2910@gmail.com>
2023-09-16 06:47:06 +0000

    Editing for Infy requirements

--- files/createWorkspace.sh
+++ files/createWorkspace.sh
@@ -1,6 +1,6 @@
 # Ensure you login to the cluster as cluster-admin
 
-for i in {2..11}
+for i in {2..3}
 do
     # Create the workspace template
     oc process -f devworkspace-template.yaml -p USERNAME=user$i | oc create -f - -n user$i-devspaces
